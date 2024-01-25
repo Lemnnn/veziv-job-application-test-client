05936a53302695af2a2f51a81d4b7e0dfb9d0bc4
Lemnul <andreilemnaru73@gmail.com>
2024-01-25 20:28:00 +0200

    Finished

--- src/components/FormDescription.jsx
+++ src/components/FormDescription.jsx
@@ -4,7 +4,7 @@ const FormDescription = ({ label, value, onChange, placeholder }) => {
       <label className="text-sm text-white font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
         {label}
         <textarea
-          className="flex w-full text-black rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[100px]"
+          className="flex w-full text-black rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[100px] max-h-[200px]"
           value={value}
           onChange={onChange}
           maxLength="250"
--- src/components/HiddenCard.jsx
+++ src/components/HiddenCard.jsx
@@ -4,10 +4,10 @@ const HiddenCard = ({ workid, hidden }) => {
   const { toggleHidden } = useData();
 
   return (
-    <div className="py-[162px] px-[100px] rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-md mx-auto h-fit">
+    <div className="flex flex-col items-center justify-center max-h-[485px] min-h-[395px] rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-md mx-auto">
       <h1 className="text-black text-center">This work is hidden</h1>
       <button
-        className="inline-flex text-white items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-black hover:bg-white/90 hover:text-black h-10 px-4 py-2 w-full"
+        className="inline-flex w-[50%] text-white items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-black hover:bg-white/90 hover:text-black h-10 px-4 py-2"
         onClick={() => toggleHidden(workid, hidden)}
       >
         Unhide
